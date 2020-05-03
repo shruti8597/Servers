b5e189f10feaf923d27a236f00e869cf08a97dc2
Shruti Agrawal <shrutiagrawal915@gmail.com>
2020-05-03 11:46:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(path.resolve(__dirname,'dist')));
 
 app.use((req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
